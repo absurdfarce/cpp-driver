42a5aa22a3543d2a069c12ac9f3654b6628a0606
absurdfarce <bret.mcguire@ibm.com>
2026-07-14 14:54:30 -0500

    CASSCPP-13 Update DRIVER_NAME after donation to ASF
    patch by Bret McGuire; reviewed by Bret McGuire and Yifan Cai

--- src/driver_info.cpp
+++ src/driver_info.cpp
@@ -29,7 +29,7 @@
 namespace datastax { namespace internal {
 
 const char* driver_name() {
-  return "DataStax C/C++ Driver for Apache Cassandra and DataStax Products";
+  return "Apache Cassandra C/C++ Driver";
 }
 
 const char* driver_version() {
