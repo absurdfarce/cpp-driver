af09abe77ae1b141f2948e2ba18bbf553d8be775
absurdfarce <bret.mcguire@ibm.com>
2026-08-26 17:34:29 -0500

    Bump max TLS protocol version up to TLS 1.3

--- src/ssl/ssl_openssl_impl.cpp
+++ src/ssl/ssl_openssl_impl.cpp
@@ -542,7 +542,7 @@ OpenSslContext::OpenSslContext()
   SSL_CTX_set_verify(ssl_ctx_, SSL_VERIFY_NONE, ssl_no_verify_callback);
 #if (OPENSSL_VERSION_NUMBER >= 0x10100000L)
   // Limit to TLS 1.2 for now. TLS 1.3 has broken the handshake code.
-  SSL_CTX_set_max_proto_version(ssl_ctx_, TLS1_2_VERSION);
+  SSL_CTX_set_max_proto_version(ssl_ctx_, TLS1_3_VERSION);
 #endif
 #if DEBUG_SSL
   SSL_CTX_set_info_callback(ssl_ctx_, ssl_info_callback);
